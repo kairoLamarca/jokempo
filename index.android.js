24db56ddb5b491909adb13b44524c754bb233a23
kairoLamarca <lamarca.kairo@gmail.com>
2018-02-22 14:45:41 -0300

    lógica para mostrar resultado
    lógica para mostrar resultado. Validando as escolhas para saber quem ganhou e mostrando na tela com o state

--- index.android.js
+++ index.android.js
@@ -12,20 +12,83 @@ class jokempo extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { escolhaUsuario: '' }
+    this.state = {
+      escolhaUsuario: '',
+      escolhaComputador: '',
+      resultado: ''
+    }
   }
 
   jokenpo(escolhaUsuario) {
-    // alert(escolhaUsuario);
-    this.setState({ escolhaUsuario: escolhaUsuario });
+
+    //gera número aleatório (0, 1, 2)
+    var numAleatorio = Math.floor(Math.random() * 3);
+
+    var escolhaComputador = '';
+
+    switch (numAleatorio) {
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+    }
+
+    var resultado = '';
+
+    if (escolhaComputador == 'pedra') {
+      if (escolhaUsuario == 'pedra') {
+        resultado = 'Empate';
+      }
+
+      if (escolhaUsuario == 'papel') {
+        resultado = 'Usuário ganhou';
+      }
+
+      if (escolhaUsuario == 'tesoura') {
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if (escolhaComputador == 'papel') {
+      if (escolhaUsuario == 'papel') {
+        resultado = 'Empate';
+      }
+
+      if (escolhaUsuario == 'tesoura') {
+        resultado = 'Usuário ganhou';
+      }
+
+      if (escolhaUsuario == 'pedra') {
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if (escolhaComputador == 'tesoura') {
+      if (escolhaUsuario == 'tesoura') {
+        resultado = 'Empate';
+      }
+
+      if (escolhaUsuario == 'pedra') {
+        resultado = 'Usuário ganhou';
+      }
+
+      if (escolhaUsuario == 'papel') {
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    this.setState({
+      escolhaUsuario: escolhaUsuario,
+      escolhaComputador: escolhaComputador,
+      resultado: resultado
+    });
   }
 
   render() {
     return (
       <View>
-        <Text>Escolha do computador</Text>
+        <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
         <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado</Text>
+        <Text>Resultado: {this.state.resultado}</Text>
         {/* deixa a função jokempo encapsulada para que ela só seja executado quando clicar */}
         <Button title="pedra" onPress={() => { this.jokenpo('pedra') }} />
         <Button title="papel" onPress={() => { this.jokenpo('papel') }} />
