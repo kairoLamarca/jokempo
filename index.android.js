bbc489b7eef5f46ee0fb0906014985b5107e8b68
kairoLamarca <lamarca.kairo@gmail.com>
2018-02-22 14:02:58 -0300

    utilização de state e setState
    utilização de state e setState para alterar o valor de um texto de um component atraves de um botão

--- index.android.js
+++ index.android.js
@@ -3,25 +3,45 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-class MeuComponente extends Component{
-  render(){
+class MeuComponente extends Component {
+  render() {
     return (
       <View>
-        <Text>{this.props.propriedade1}</Text>
+        {/* <Text>{this.props.propriedade1}</Text>
         <Text>{this.props.xyz}</Text>
-        <Text>{this.props.p}</Text>
-      </View>      
+        <Text>{this.props.p}</Text> */}
+        <Text>{this.props.teste}</Text>
+      </View>
     )
   }
 }
 
-class jokempo extends Component{
-  render(){
-    return(
-      <MeuComponente propriedade1='Banana' xyz='Abacaxi' p='Uva'></MeuComponente>
+class jokempo extends Component {
+
+  constructor(props) {
+    super(props);//passa as propriedades para o Component
+
+    this.state = { texto: 'Texto teste 2' };
+  }
+
+  alteraTexto() {
+    this.setState({ texto: 'Outra coisa' });//fun;'ao herdada do Component
+  }
+
+  render() {
+    return (
+      <View>
+        {/* <MeuComponente propriedade1='Banana' xyz='Abacaxi' p='Uva'></MeuComponente> */}
+        <MeuComponente teste={this.state.texto}></MeuComponente>
+        <Button
+          title='Botão'
+          onPress={() => { this.alteraTexto() }}
+        />
+      </View>
     );
   }
 }
