5bb37131ec8d9fc7465d07b4be0d3f2eab8f96e6
kairoLamarca <lamarca.kairo@gmail.com>
2018-02-27 13:35:21 -0300

    estilos e adição de imagens das escolhas
    estilos e adição de imagens das escolhas

--- index.android.js
+++ index.android.js
@@ -103,11 +103,16 @@ class jokempo extends Component {
           </View>
 
         </View>
-        <View></View>
 
-        <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
-        <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado: {this.state.resultado}</Text>
+        <View style={styles.palco}>
+          <Text style={styles.txtResultado}>{this.state.resultado}</Text>
+          <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
+          <Image source={require('./imgs/tesoura.png')}/>
+          
+          <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
+          <Image source={require('./imgs/tesoura.png')}/>
+
+        </View>
 
       </View>
     );
@@ -132,6 +137,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 10
+  },
+  palco: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  txtResultado:{
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'red',
+    height: 60
   }
 });
 
